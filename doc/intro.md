98942751ad3282355eb1b1bbb3e4b3265cefc8ce
olga.leao <olga.leao@gmail.com>
2021-11-24 16:55:30 -0400

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ysc-challenge-w1
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ysc_challenge_w1/core_test.clj
@@ -0,0 +1,7 @@
+(ns ysc-challenge-w1.core-test
+  (:require [clojure.test :refer :all]
+            [ysc-challenge-w1.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
